544fa96c91871c3a7edc2771b9f0e7f0d7e2b123
Maru Khan <romirov@gmail.com>
2023-04-11 00:15:19 +0300

    added webpack

--- index.js
+++
@@ -1,4 +0,0 @@
-import _ from './node_modules/lodash';
-
-console.log("TEST")
-console.log(_.random(1, 10))
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles/style.css"
+import "./styles/fonts.css"
+import random from 'lodash/random';
+import _ from 'lodash';
+
+console.log("TEST")
+console.log(random(1, 10))
+console.log(random(20, 100))
